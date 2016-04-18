9959b8c84db2179cc8b8dab78da7c868a00742b2
Martin Derouet <martin.derouet0@gmail.com>
2016-04-18 09:36:18 +0200

    Ajout dans mon tableau

--- Reminder/Settings.swift
+++ Reminder/Settings.swift
@@ -10,7 +10,7 @@ import UIKit
 import EventKit
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
-    let myArray = ["Notification", "Option 2", "Option 3"]
+    let myArray = ["Notification", "Option 2", "Option 3","Option 4"]
         //Outlet
     @IBOutlet weak var tableReminder: UITableView!
     
